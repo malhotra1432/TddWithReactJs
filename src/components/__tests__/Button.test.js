7e5eae5fde98a94930644923ce078f65754587e0
Prabhat Malhotra <malhotra1432@gmail.com>
2020-08-13 00:25:20 +0530

    [Prabhat] tests lint and prettier

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import {shallow} from "enzyme";
+import { shallow } from 'enzyme';
 import Button from '../button/Button';
 
-describe("Button", () => {
-    let mountedButton;
-    beforeEach(() => {
-        mountedButton = shallow(<Button/>)
-    });
+describe('Button', () => {
+  let mountedButton;
+  beforeEach(() => {
+    mountedButton = shallow(<Button />);
+  });
 
-    it('should render Store Locator component', () => {
-        let mountedButton = shallow(<Button/>);
-    });
+  it('should render Store Locator component', () => {
+    let mountedButton = shallow(<Button />);
+  });
 
-    it('should render a button', () => {
-        const button = mountedButton.find('button');
-        expect(button.length).toBe(1);
-    });
+  it('should render a button', () => {
+    const button = mountedButton.find('button');
+    expect(button.length).toBe(1);
+  });
+});
 
+describe('When a location is passed to it', () => {
+  let mountedButton;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      location: 'location1',
+    };
+    mountedButton = shallow(<Button {...props} />);
+  });
 });
